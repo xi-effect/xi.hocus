0487ceb6b4816d586f5eaacfec560bcdb005e970
niqzart <qwert45hi@yandex.ru>
2024-09-23 00:52:23 +0300

    feat: access check action

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { Server } from "@hocuspocus/server"
-import { Doc } from "yjs"
+
+import { verifyYDocAccess } from "./hooks/access"
 
 const server = Server.configure({
-    name: "xi.hocus",
-    port: 1234,
-    timeout: 30000,
-    debounce: 5000,
-    maxDebounce: 30000,
-    quiet: true,
+  name: "xi.hocus",
+  port: 1234,
+  timeout: 30000,
+  debounce: 5000,
+  maxDebounce: 30000,
+  quiet: true,
+  onAuthenticate: verifyYDocAccess,
 })
 
 server.listen()
